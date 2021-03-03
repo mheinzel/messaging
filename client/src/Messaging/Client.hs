35899ea63a14544c943d58163c64ee11c196efbb
Matthias Heinzel <mheinzel@posteo.de>
2021-03-03 14:00:51 +0100

    ormolu

--- client/src/Messaging/Client.hs
+++ client/src/Messaging/Client.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Messaging.Client where
 
-import           Control.Monad       (unless)
-import           Network.Socket      (withSocketsDo)
-import           Data.Text           (Text)
-import qualified Data.Text           as T
-import qualified Data.Text.IO        as T
-import qualified Network.WebSockets  as WS
+import Control.Monad (unless)
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Network.Socket (withSocketsDo)
+import qualified Network.WebSockets as WS
 
 runClient :: IO ()
 runClient = do
@@ -15,16 +16,16 @@ runClient = do
 
 client :: WS.ClientApp ()
 client conn = do
-    putStrLn "Connected!"
+  putStrLn "Connected!"
 
-    -- Read from stdin and write to WS
-    let loop = do
-            line <- T.getLine
-            unless (T.null line) $ do
-              WS.sendTextData conn line
-              msg <- WS.receiveData conn
-              T.putStrLn msg
-              loop
+  -- Read from stdin and write to WS
+  let loop = do
+        line <- T.getLine
+        unless (T.null line) $ do
+          WS.sendTextData conn line
+          msg <- WS.receiveData conn
+          T.putStrLn msg
+          loop
 
-    loop
-    WS.sendClose conn ("Bye!" :: Text)
+  loop
+  WS.sendClose conn ("Bye!" :: Text)
--- server/src/Messaging/Server.hs
+++ server/src/Messaging/Server.hs
@@ -1,8 +1,8 @@
 module Messaging.Server where
 
-import           Control.Monad       (forever)
-import qualified Data.Text.IO        as T
-import qualified Network.WebSockets  as WS
+import Control.Monad (forever)
+import qualified Data.Text.IO as T
+import qualified Network.WebSockets as WS
 
 runApp :: IO ()
 runApp = do
@@ -11,10 +11,10 @@ runApp = do
 
 app :: WS.ServerApp
 app pending = do
-    conn <- WS.acceptRequest pending
-    WS.withPingThread conn 30 (return ()) $ do
-      putStrLn "Connected!"
-      forever $ do
-          msg <- WS.receiveData conn
-          T.putStrLn msg
-          WS.sendTextData conn msg
+  conn <- WS.acceptRequest pending
+  WS.withPingThread conn 30 (return ()) $ do
+    putStrLn "Connected!"
+    forever $ do
+      msg <- WS.receiveData conn
+      T.putStrLn msg
+      WS.sendTextData conn msg

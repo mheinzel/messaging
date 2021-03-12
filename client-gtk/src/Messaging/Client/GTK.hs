d99f31fb7a47a1f9e944d5639491452d4ed0e419
xiving <b.f.remmers@students.uu.nl>
2021-03-12 20:08:40 +0100

    Trying out GTK

--- client-gtk/src/Messaging/Client/GTK.hs
+++ client-gtk/src/Messaging/Client/GTK.hs
@@ -3,16 +3,20 @@
 
 module Messaging.Client.GTK where
 
+import Control.Monad (void)
 import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text (encodeUtf8)
+import GI.Gtk.Declarative
+import GI.Gtk.Declarative.App.Simple
 import qualified Messaging.Client.Core.Connection as Conn
-import qualified Messaging.Client.GTK.UI as UI ()
+import qualified Messaging.Client.GTK.UI as UI
 import Messaging.Shared.User (mkUserName, userNameText)
 import Network.Socket (withSocketsDo)
 import qualified Network.WebSockets as WS
 import qualified System.Environment as Env
 import qualified System.Exit as Exit
+import Control.Concurrent (newChan)
 
 runClient :: IO ()
 runClient = do
@@ -39,7 +43,21 @@ client conn = do
   _outgoing <- Conn.spawnSendThread conn
 
   -- TODO: run UI
-  pure ()
+  runUI
 
   -- We might want to also explicitly leave our conversations here.
   WS.sendClose conn ("Bye!" :: Text)
+
+
+-- as is, takes input and crashes when m is pressed
+runUI :: IO ()
+runUI = do
+  input <- newChan
+  void $
+    run
+      App
+        { view = UI.view,
+          update = UI.update,
+          inputs = input,
+          initialState = UI.initialState
+        }
--- client-gtk/src/Messaging/Client/GTK/UI.hs
+++ client-gtk/src/Messaging/Client/GTK/UI.hs
@@ -1,1 +1,61 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Messaging.Client.GTK.UI where
+
+import Control.Concurrent.Chan (Chan)
+import GI.Gdk (EventKey, getEventKeyString)
+import GI.Gtk
+  ( Box (..),
+    Entry (..),
+    Orientation (..),
+    Window (..),
+  )
+import GI.Gtk.Declarative
+import GI.Gtk.Declarative.App.Simple
+
+data State = Initial
+
+data Event
+  = Closed
+  | Ignore
+
+-- | The update function of an application reduces the current state and
+-- a new event to a 'Transition', which decides if and how to transition
+-- to the next state.
+update :: State -> Event -> Transition State Event
+update _ Closed = Exit
+update s _ = Transition s $ pure Nothing
+
+-- | The view renders a state value as a window, parameterized by the
+-- 'App's event type.
+view :: State -> AppView Window Event
+view s =
+  bin
+    Window
+    [ #title := "Title",
+      on #deleteEvent (const (True, Closed)),
+      #widthRequest := 400,
+      #heightRequest := 300
+    ]
+    $ container
+      Box
+      [#orientation := OrientationVertical]
+      [widget Entry [onM #keyPressEvent windowKeyPressEventHandler]]
+
+windowKeyPressEventHandler :: EventKey -> Entry -> IO (Bool, Event)
+windowKeyPressEventHandler eventKey _ = do
+  key <- getEventKeyString eventKey
+  case key of
+    Just "m" -> return (True, Closed)
+    _ -> return (False, Ignore)
+
+-- | Inputs come through a channel that feed events into the application.
+inputs :: Chan Event
+inputs = undefined
+
+-- | The initial state value of the state reduction loop.
+initialState :: State
+initialState = Initial

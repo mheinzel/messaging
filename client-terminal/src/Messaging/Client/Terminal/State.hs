b27d4287233a835fb9b90193874f345aa0ea5b7b
Matthias Heinzel <mheinzel@posteo.de>
2021-03-22 17:05:06 +0100

    input demo app

--- client-terminal/src/Messaging/Client/Terminal/State.hs
+++ client-terminal/src/Messaging/Client/Terminal/State.hs
@@ -13,33 +13,14 @@ import qualified System.Console.ANSI.Declarative.Input as Ansi
 import qualified System.Console.ANSI.Declarative.Widget as Widget
 
 data State = State
-  { _coreState :: Core.State,
-    _editor :: Widget.Editor,
-    _sidebarExpanded :: Bool,
-    _unicodeEnabled :: Bool
+  { _inputs :: [Ansi.KeyboardInput]
   }
   deriving (Show)
 
 makeLenses ''State
 
-currentConversationName :: State -> Conv.ConversationName
-currentConversationName =
-  Core._conversationName . Core._currentConversation . _coreState
-
-handleEditorInput :: Ansi.KeyboardInput -> State -> State
-handleEditorInput input = over editor (Widget.handleInput input)
-
-resetEditor :: State -> State
-resetEditor = set editor (Widget.editor "")
-
-editorContent :: State -> [Text]
-editorContent = Widget.editorContent . _editor
-
-toggleSidebar :: State -> State
-toggleSidebar = over sidebarExpanded not
-
-toggleUnicode :: State -> State
-toggleUnicode = over unicodeEnabled not
+addInput :: Ansi.KeyboardInput -> State -> State
+addInput i = over inputs (i :)
 
 initialState :: State
-initialState = State Core.emptyState (Widget.editor "") True False
+initialState = State []
--- client-terminal/src/Messaging/Client/Terminal/UI.hs
+++ client-terminal/src/Messaging/Client/Terminal/UI.hs
@@ -19,74 +19,18 @@ import qualified System.Console.ANSI.Declarative.Simple as Simple
 import qualified System.Console.ANSI.Declarative.Widget as Widget
 
 runUI :: Chan Res.Response -> Chan Req.Request -> IO ()
-runUI incomingChan outgoingChan = do
-  () <$ Simple.runApp (app incomingChan outgoingChan)
+runUI _incomingChan _outgoingChan = do
+  () <$ Simple.runApp app
 
-app :: Chan Res.Response -> Chan Req.Request -> Simple.App Widget.SomeWidget State Event
-app incomingChan outgoingChan =
+app ::  Simple.App Widget.SomeWidget State Input.KeyboardInput
+app =
   Simple.App
-    { Simple.update = handleEvent outgoingChan,
+    { Simple.update = handleEvent,
       Simple.view = const viewState,
-      Simple.events =
-        [ ServerResponse <$> readChan incomingChan,
-          Input <$> Input.readInput,
-          Tick <$ Input.waitForTickMilliseconds 1000
-        ],
+      Simple.events = [ Input.readInput],
       Simple.initialState = initialState
     }
 
-data Event
-  = ServerResponse Res.Response
-  | Input Input.KeyboardInput
-  | -- | This makes sure we occasionally re-render the UI, e.g. after resizing
-    -- the terminal.
-    Tick
-  deriving (Show)
-
-handleEvent :: Chan Req.Request -> State -> Event -> Simple.Transition State
-handleEvent outgoingChan state = \case
-  Tick -> Simple.Transition $ pure state
-  ServerResponse res ->
-    Simple.Transition $
-      pure $ over coreState (Core.handleServerResponse res) state
-  Input Input.Escape -> Simple.Exit
-  Input Input.Tab -> Simple.Transition $ do
-    pure $ toggleSidebar state
-  Input Input.Enter ->
-    case typedCommand state of
-      Just CmdQuit -> Simple.Exit
-      Just CmdSidebar -> Simple.Transition $ do
-        pure $ resetEditor $ toggleSidebar state
-      Just CmdUnicode -> Simple.Transition $ do
-        pure $ resetEditor $ toggleUnicode state
-      Just (CmdSend txt) -> Simple.Transition $ do
-        let convName = currentConversationName state
-        let msg = Msg.Message convName txt
-        writeChan outgoingChan (Req.SendMessage msg)
-        pure $ resetEditor state
-      Nothing -> Simple.Transition $ do
-        -- Don't send or delete if no command was detected.
-        pure state
-  Input kb -> Simple.Transition $ do
-    pure $ handleEditorInput kb state
-
-data Command
-  = CmdQuit
-  | CmdSidebar
-  | CmdUnicode
-  | CmdSend Text
-
-typedCommand :: State -> Maybe Command
-typedCommand = command . Text.strip . Text.unlines . editorContent
-  where
-    command txt
-      | Text.null txt = Nothing
-      | Text.isPrefixOf "/" txt = findCommand txt
-      | otherwise = Just (CmdSend txt)
-
-    findCommand txt =
-      fmap snd . List.find (flip Text.isPrefixOf txt . fst) $
-        [ ("/quit", CmdQuit),
-          ("/sidebar", CmdSidebar),
-          ("/unicode", CmdUnicode)
-        ]
+handleEvent :: State -> Input.KeyboardInput -> Simple.Transition State
+handleEvent state input=
+  Simple.Transition $ pure $ addInput input state

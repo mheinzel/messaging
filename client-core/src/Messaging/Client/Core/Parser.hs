551d60aa6029e5d39562934f0d6c62a551a9fb1a
Nathan Oerlemans <n.h.k.oerlemans@students.uu.nl>
2021-04-05 19:18:40 +0200

    Minor changes in Parser

--- client-core/src/Messaging/Client/Core/Parser.hs
+++ client-core/src/Messaging/Client/Core/Parser.hs
@@ -1,40 +1,44 @@
-{-# LANGUAGE QuasiQuotes #-}
-
 module Messaging.Client.Core.Parser where
 
 import Options.Applicative
 import Data.Semigroup ((<>))
 import Messaging.Shared.User as User
-import Data.Text (pack)
+import Data.Text (Text, pack)
 import Options.Applicative.Types
 import qualified Messaging.Client.Core.Connection as Con
 import qualified URI.ByteString as URI
 import qualified Data.ByteString.Char8 as Byte
 
 data ConnectInput = ConnectInput 
-  { username    :: User.UserName 
-  , uri         :: Con.URI}
+  { _username    :: Text -- Parsing to text and convert in client code
+  , _uri         :: Con.URI}
 
 parseConnectInput :: Parser ConnectInput
 parseConnectInput = ConnectInput
       <$> option userReadM
           ( long "username"
+         <> short 'u'
          <> metavar "NAME"
          <> help "Name for the username" )
       <*> option uriReadM
           ( long "uri"
          <> metavar "TARGET"
-         <> help "Target for the uri" )
+         <> help "Target is the uri" )
 
-userReadM :: ReadM User.UserName 
-userReadM = User.UserName . pack <$> readerAsk
+userReadM :: ReadM Text
+userReadM = pack <$> readerAsk
 
 uriReadM :: ReadM Con.URI 
 uriReadM = stringToUri <$> readerAsk
 
 stringToUri :: String -> Con.URI
 stringToUri s = case Con.uriFromAbsolute uriLoc  of
-            Right u -> u
+            Right uri -> uri
             Left err -> error $ "Unexpected invalid default URI: " <> err
         where uriLoc = either (error . show) id (URI.parseURI URI.laxURIParserOptions (Byte.pack s))
 
+runParse :: IO ConnectInput
+runParse = execParser $ info (parseConnectInput <**> helper)
+            ( fullDesc
+     <> progDesc "Parses urename and URI"
+     <> header "Takes a username -u \"username\" and URI -u \"URI\" and returns them." )

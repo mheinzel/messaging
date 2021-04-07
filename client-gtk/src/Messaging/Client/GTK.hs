15ff2df6717b45cdfe70e8f0a1b2ab93763b8845
Nathan Oerlemans <n.h.k.oerlemans@students.uu.nl>
2021-04-07 14:10:08 +0200

    Clean up gtk file after rebase

--- client-gtk/src/Messaging/Client/GTK.hs
+++ client-gtk/src/Messaging/Client/GTK.hs
@@ -1,14 +1,10 @@
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Messaging.Client.GTK where
 
-import qualified Data.Text as Text
 import qualified Messaging.Client.Core.Connection as Conn
 import qualified Messaging.Client.GTK.UI as UI
-import qualified Messaging.Shared.User as User
 import qualified Network.WebSockets as WS
-import qualified System.Environment as Env
 import qualified System.Exit as Exit
 import qualified Messaging.Shared.User as User
 import qualified Messaging.Client.Core.Parser as Par
@@ -16,12 +12,11 @@ import qualified Messaging.Client.Core.Parser as Par
 runClient :: IO ()
 runClient = do
   input <- Par.runParse
-  userName <- case mkUserName (Par._username input) of
+  userName <- case User.mkUserName (Par._username input) of
         Just userName -> pure userName
         Nothing -> Exit.die "error: invalid user name"
-  uri <- pure $ Par._uri input 
   
-  Conn.runClientApp uri userName (client userName)
+  Conn.runClientApp (Par._uri input) userName (client userName)
 
 client :: User.UserName -> WS.ClientApp ()
 client userName conn = do
